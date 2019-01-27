bcb6cf179f8a37c6c1a0dde3afc2505eba2bfa11
Kirsten Sugar <xosugar443@gmail.com>
2019-01-26 21:19:32 -0500

    Implemented Nest Class

---
+++ Assets/Scripts/Nest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Nest : MonoBehaviour
+{
+	private SpriteRenderer spriteRenderer;
+	[SerializeField] private Sprite[] progressSprites;
+
+	private void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+		SetProgress(0f);
+	}
+
+	public void SetProgress (float progress) 
+	{
+		// SET IMAGE OF PROGRESS BASED ON PROGRESS
+
+		if (progressSprites.Length > 0)
+		{
+			int index = Mathf.FloorToInt(progressSprites.Length / progress);
+
+			spriteRenderer.sprite = progressSprites[index];
+		}
+	}
+}
